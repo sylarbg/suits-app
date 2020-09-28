75490524f477930938de93ae56ac015606a72797
Denis <dgeorgiev.web@gmail.com>
2020-09-28 20:30:17 +0300

    Build for deploy

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git remote add origin https://github.com/sylarbg/suits-app.git
+#git push -u origin build
+git subtree push --prefix dist origin build
+
+#git push origin build
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
